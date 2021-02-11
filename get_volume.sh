7bb9eda981a6fa6ca89169e5066ec355cc1c72de
Adam Podkowinski <adampodkev@gmail.com>
2021-02-11 12:25:11 +0100

    Change get volume scripts

--- get_volume.sh
+++ get_volume.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo $(amixer sget Master | grep off -q && echo MUTED || pactl list sinks | grep '^[[:space:]]Volume:' | \
-  head -n $(( $SINK + 2 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%
+echo $(amixer sget Master | grep off -q && echo MUTED || pactl list sinks | grep -e analog-stereo -e Volume: | cut -d' ' -f6 | cut -d'/' -f1)
+
+
