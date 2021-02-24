1adbdb429bd20c0fbdd0926b0220925872f1a361
Adam Podkowinski <adampodkdev@gmail.com>
2021-02-24 09:58:46 +0100

    Add neat scripts

--- change_vol.sh
+++ change_vol.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pactl set-sink-volume @DEFAULT_SINK@ $1
-notify-send -t 300 $(/home/bodzio/scripts/get_volume.sh) &
+dunstctl close-all
+notify-send $(/home/bodzio/scripts/get_volume.sh) &
