00753c60e2b831856a991483278369df613744ab
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-31 10:56:37 +0200

    Add screenshots

--- scrot_clip.sh
+++ scrot_clip.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-scrot -s /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
+#!/bin/bash
+if [ "$1" == "pick" ]
+then
+  scrot -s /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
+else
+  scrot /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
+fi
 
 notify-send "Screenshot copied to clipboard" -t 2000
