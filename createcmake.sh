ce1e3ae6b9b40317eda824f7d57342766d0df40b
Adam Podkowinski <adampodkdev@gmail.com>
2021-04-08 22:16:30 +0200

    Finish createcmake.sh

--- createcmake.sh
+++ createcmake.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/adam-podkowinski/cmaketemplate $1
-rm $1/.git -r
+git clone --depth 1 https://github.com/adam-podkowinski/cmaketemplate $1
+rm -rf $1/.git 2> /dev/null
+cd $1
+git init
+git add .
+git commit -m "Initial commit"
