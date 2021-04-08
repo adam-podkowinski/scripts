9f64e112be88cc51696849ab26a20da4e7d14392
Adam Podkowinski <adampodkdev@gmail.com>
2021-04-08 22:07:00 +0200

    Make a createcmake script

---
+++ createcmake.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/adam-podkowinski/cmaketemplate $1
+rm $1/.git -r
--- scrot_clip.sh
+++ scrot_clip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" == "pick" ]
 then
-  scrot -s /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
+  scrot -sp /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
 else
-  scrot /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
+  scrot -p /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'
 fi
 
 notify-send "Screenshot copied to clipboard" -t 2000
