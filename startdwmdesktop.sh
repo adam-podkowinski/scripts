b29adb2abc35ff12460f4cbfeb11573e5fd1acbe
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-07-01 19:32:17 +0200

    Add startdwmdesktop.sh file

---
+++ startdwmdesktop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xset r rate 199 30
+nitrogen --restore &
+slstatus &
+/usr/lib/polkit-kde-authentication-agent-1 &
+dunst &
+setxkbmap pl
+xmodmap ~/.config/xmodmap
+export QT_QPA_PLATFORMTHEME=gtk2
+picom -b
+xinput --set-prop 'pointer:COMPANY USB Device' 'libinput Accel Profile Enabled' 0 1
+exec dwm
