55617dcbeff5cfacc987951141b23007fc664e96
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-07-01 12:52:38 +0200

    LOL

--- change_vol.sh
+++ change_vol.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pulsemixer --change-volume $1 &
 #dunstctl close-all &
-dunstify -t 200 -i audio-volume-muted-blocking -r 1 $(get_volume.sh) &
+#dunstify -t 200 -i audio-volume-muted-blocking -r 1 $(get_volume.sh) &
---
+++ startdwm.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+desktop=dwm startx
