406fd360f3cd15477f5b894a478a7383ed01f4a4
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-12-26 21:35:39 +0100

    Fix vnc.sh script

--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+killall x11vnc
+doas modprobe -r evdi
+doas modprobe evdi initial_device_count=2
 xrandr --setprovideroutputsource 1 0 --setprovideroutputsource 2 0
-xrandr --addmode DVI-I-1-1 1280x800
-xrandr --output DVI-I-1-1 --mode 1280x800 --left-of HDMI-0
-x11vnc -repeat -clip 1280x800-1920-280
+OUTPUT=$(xrandr | grep DVI-I-1-. | cut -d\  -f1)
+xrandr --addmode $OUTPUT 1280x800
+xrandr --output $OUTPUT --mode 1280x800 --left-of HDMI-0
+nitrogen --restore &
+x11vnc -repeat -clip 1280x800-1920-280 -forever -noxdamage -nosel -notruecolor & disown
