31ae6e3ca8b2a3980583919a0d2fd8680bb2affd
adampodkdev@gmail.com <adampodkdev@gmail.com>
2022-05-07 14:51:39 +0200

    Swap caps to escape

--- startdwmdesktop.sh
+++ startdwmdesktop.sh
@@ -6,6 +6,7 @@ slstatus &
 dunst &
 /usr/bin/pipewire & /usr/bin/pipewire-pulse & /usr/bin/pipewire-media-session &
 setxkbmap pl
+setxkbmap -option caps:swapescape
 xmodmap ~/.config/xmodmap
 picom -b
 xinput --set-prop 'pointer:COMPANY USB Device' 'libinput Accel Profile Enabled' 0 1
