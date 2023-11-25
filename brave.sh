9f020bd0ac19c1b693f5af125842e860b2e251c7
Adam Podkowinski <adampodkdev@gmail.com>
2023-11-25 02:11:37 +0100

    brave.sh

--- brave.sh
+++ brave.sh
@@ -1,1 +1,2 @@
-brave-nightly --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
+brave-nightly --disable-features=WaylandFractionalScaleV1 --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
+# brave-nightly --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
