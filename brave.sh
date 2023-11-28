a5f94adc4911319f37e0a3ccb5e246affcaf4c74
Adam Podkowinski <adampodkdev@gmail.com>
2023-11-28 22:15:15 +0100

    brave and sysact

--- brave.sh
+++ brave.sh
@@ -1,2 +1,3 @@
-brave-nightly --disable-features=WaylandFractionalScaleV1 --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
-# brave-nightly --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
+# brave-nightly --disable-features=WaylandFractionalScaleV1 --enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
+brave-nightly --enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization $1
+# brave-nightly $1 $2
